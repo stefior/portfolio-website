fe10b11884356d4a9b80ae9d8249aeb00d560466
stefior <stephencfiorenza@gmail.com>
2023-07-11 16:45:35 -0400

    feat: replace char restaurant with mizners dream project

--- dist/serviceworker.js
+++ dist/serviceworker.js
@@ -1,21 +1,21 @@
-const cacheName = "cache-v1"; 
+// const cacheName = "cache-v1"; 
 
-const filesToCache = [
-  '/',
-];
+// const filesToCache = [
+//   '/',
+// ];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+// self.addEventListener('install', function(e) {
+//   e.waitUntil(
+//     caches.open(cacheName).then(function(cache) {
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function(e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function(response) {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });
